620b80a379932b1077bfdab749bd016cff9ab3bf
diego6bravo <diego.bravo@live.com>
2025-09-26 01:44:01 -0500

    reordenando carpetas

--- clase24_javascript/objetos.js
+++
@@ -1,6 +0,0 @@
-let nombre= "Diego"
-let raza="Demon Slayer"
-
-let personaje = {
-    let nombre: carlos; 
-}
---
+++ recursos_curso/clase24_javascript/confirmacion.js
@@ -0,0 +1,31 @@
+let nombre = prompt("¿cuál es su nombre?")
+let apellido = prompt("¿cuál es su apellido?")
+let edad = prompt("¿cuál es su edad?")
+let confirmar = confirm ("¿Desea guardar la info?")
+console.log(nombre, apellido, edad, confirm);
+
+
+
+// ¿Cuál de las siguientes opciones representa correctamente el uso de scope global y local?  
+
+// //opcion A
+// let saludo = 'hola';
+// function saludar() {
+    
+//     saludo='Buenos dias';
+//     console.log(saludo);
+
+// }
+// saludar();
+
+
+
+// //opcion B
+// let saludo = 'hola';
+// function saludar() {
+    
+//     let mensaje = 'Buenos dias';
+//     console.log(saludo + ' ' + mensaje);
+    
+// }
+// saludar();
